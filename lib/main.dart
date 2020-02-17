a2c4f4dd52d72cfecdbf82d42a928aa3dcfb11af
yahya997 <yahya.1997914@gmail.com>
2020-02-17 22:20:31 +0200

    ""

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:news_application/screens/home_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'screens/OnBoarding.dart';
 
-import 'OnBoarding.dart';
-
-main(){
-  runApp(newsApp());
+main() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool seen = prefs.getBool( 'seen' );
+  Widget _screen;
+  if( seen == null || seen == false ){
+    _screen = OnBoarding();
+  }else{
+    _screen = HomeScreen();
+  }
+  runApp( NewsApp(_screen));
 }
 
-class newsApp extends StatelessWidget {
+class NewsApp extends StatelessWidget {
+
+  Widget _screen;
+
+  NewsApp(this._screen);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: OnBoarding()
+      home: this._screen,
     );
   }
 }
